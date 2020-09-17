328f5695e522dfe6488252da4d0776ffa8571461
German Cruz <cruzgerman216@yahoo.com>
2020-09-17 07:52:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function renderBreeds(breeds){
   const main = document.querySelector('body');
 
   for (const breed in object){
+    console.log(breed.length);
     if breed.length != 0 {
       const list = document.createElement('li');
       main.appendChild(list);
