b04c2515ba8aedd07145c13347fdd8dfa297745b
German Cruz <cruzgerman216@yahoo.com>
2020-09-17 07:23:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function getImages() {
+  return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
+
+function renderImages(json){
+  
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
